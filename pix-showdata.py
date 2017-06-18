41654f77e7bdcd27849986d5a44c275bb161ae62
AhmedRejeb <rejebahmed6@gmail.com>
2017-06-18 14:24:42 +0200

    Update pix-showdata.py

--- pix-showdata.py
+++ pix-showdata.py
@@ -21,9 +21,9 @@ from modules.pixVehicle import *
 # SITL via TCP
 #vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
 # SITL via UDP 
-vehicle = connect('udp:127.0.0.1:14549', wait_ready=True)
+#vehicle = connect('udp:127.0.0.1:14549', wait_ready=True)
 # Real vehicle via Serial Port 
-#vehicle = connect('/dev/tty.usbmodem1', wait_ready=False)
+vehicle = connect('/dev/tty.usbmodem1', wait_ready=False)
 
 while True:
 	print "%s" % vehicle.attitude #SR2_EXTRA1
@@ -32,8 +32,8 @@ while True:
 	print "Altitude (global frame): %s" % vehicle.location.global_frame.alt
 	print "Altitude (global relative frame): %s" % vehicle.location.global_relative_frame.alt
 	print "%s" % vehicle.mode.name
-	#print "%s" % udp.message
-	time.sleep(0.01)
+	print "%s" % udp.message
+	time.sleep(0.1)
 
 vehicle.close()
 
