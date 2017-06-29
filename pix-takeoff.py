4ca01694763bbbf13c0a0cbb0906b9d852d681c2
AhmedRejeb <rejebahmed6@gmail.com>
2017-06-29 17:13:42 +0200

    Update pix-takeoff.py

--- pix-takeoff.py
+++ pix-takeoff.py
@@ -21,9 +21,9 @@ from modules.pixVehicle import *
 # SITL via TCP
 #vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
 # SITL/vehicle via UDP (connection coming from mavproxy.py)
-vehicle = connect('udp:127.0.0.1:14550', wait_ready=True)
+#vehicle = connect('udp:127.0.0.1:14550', wait_ready=True)
 # Direct UART communication to Pixhawk
-#vehicle = connect('/dev/ttyAMA0', wait_ready=True)
+vehicle = connect('/dev/serial0',baud=56700, wait_ready=False)
 
 """ Mission starts here """
 
