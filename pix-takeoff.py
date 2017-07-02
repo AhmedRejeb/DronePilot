3276083f0fe67ff9c50b0ef0d12dde193d0fa227
AhmedRejeb <rejebahmed6@gmail.com>
2017-07-02 03:22:46 +0200

    Update pix-takeoff.py

--- pix-takeoff.py
+++ pix-takeoff.py
@@ -14,6 +14,7 @@ __status__ = "Development"
 import time
 from dronekit import connect, VehicleMode
 #import modules.UDPserver as udp
+from modules import *
 from modules.utils import *
 from modules.pixVehicle import *
 
